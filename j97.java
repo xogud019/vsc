c073ba3f226a23bd18f1d2cd79af4f7b4b2f6ef3
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-01-29 11:25:40 +0900

    commit

--- j97.java
+++ j97.java
@@ -5,3 +5,42 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TalbeView;
 import javafx.scene.web.WebView;
+
+public class j97 /*BillboardfxController*/{
+    @FXML
+    private TableVeiw<Billboard> billboardTable;
+    
+    @FXML
+    private TableColumn<Billboard,String> song;
+
+    @FXML
+    private TableColumn<Billboard,String> artist;
+    
+    @FXML
+    private TableColumn<Billboard,Integer> lastWeek;
+    
+    @FXML
+    private TableColumn<Billboard,Integer> rank;
+    
+    @FXML
+    private TableColumn<Billboard,String> imagesrc;
+    
+    @FXML
+    private WebView billwebview;
+
+    @FXML
+    private Button btnAf;
+
+    @FXML
+    private Button btnBf;
+
+    @FXML
+    private Label lbbill;
+
+    @FXML
+    void onClickBtnBf(ActionEvent event){}
+
+    @FXML
+    void onClickBtnAf(ActionEvent event){}
+
+}
