a5e3c312926afde9b36d73dff79e2e29b2c7ab39
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-02-29 22:16:49 +0900

    commit

---
+++ Kakao/failureMain.java
@@ -0,0 +1,50 @@
+//19 kakao 2
+import java.util.Scanner;
+
+public class failureMain {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			System.out.print("input Num(1~500) : ->");
+			int N= scan.nextInt();
+		
+			if(N>500) {
+				System.out.println("Num must under the 500");
+				System.out.println("progran end");
+				break;
+			}
+			
+			System.out.print("input stage`num(1~200,000) : ->");
+			int stageNum= scan.nextInt();
+			
+			if(stageNum>200000) {
+				System.out.println("stage`num must under the 200,000");
+				System.out.println("progran end");
+				break;
+			}
+			
+			int[] stages = new int[stageNum];
+			for(int i=0; i<stages.length; i++) {
+				stages[i] = (int)(Math.random()*(N+1))+1;
+			}
+			for(int i=0; i<stages.length-1; i++) {
+				System.out.print(stages[i]+",");
+			}
+			System.out.println(stages[stageNum-1]);
+			double[] temp = new double[stageNum];
+			failureRate fR = new failureRate(N);
+			temp = fR.fRate(N, stages);
+			for(int i=0; i<temp.length-1; i++) {
+				System.out.print(temp[i]+", ");
+			}
+			System.out.println(temp[N-1]);
+			
+			fR.sort(temp);
+			for(int i=0; i<temp.length; i++) {
+				System.out.print(temp[i]+" ");
+			}
+			System.out.println();
+			fR.print();
+		}
+	}
+}
---
+++ Kakao/failureRate.java
@@ -0,0 +1,73 @@
+//19 kakao 2
+public class failureRate {
+	int[] stages;
+	double[] failRate;
+	
+	public failureRate(int n) {
+		this.stages = new int[n];
+		for(int i=0; i<n; i++) {
+			this.stages[i]=(i+1);
+		}
+		this.failRate = new double[n];
+	}
+	
+	public void print() {
+		for(int i=0; i<stages.length; i++) {
+			System.out.print(stages[i]+" ");
+		}
+	}
+	
+	public double[] fRate(int n, int[] stages) {
+		int count=0;
+		int totalNum = stages.length;
+		double temp= 0;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<stages.length; j++) {
+				if(stages[j]==(i+1)) {
+					count++;
+				}
+			}
+			if(totalNum==0&&i==(n-1)) {
+				failRate[i]=0;
+				
+			}
+			else {
+				temp = (double)count/(double)totalNum;
+				totalNum -=count;
+				count=0;
+				failRate[i]=temp;
+		
+			}
+		}
+		return failRate;
+	}
+	
+	public void sort(double[] n){
+        for(int i=0; i<n.length;i++){
+            for(int j=0; j<n.length-1;j++){
+                double temp;
+                int temp1;
+                if(n[j]<n[j+1]){
+                    temp= n[j];
+                    temp1 = stages[j];
+                    n[j] = n[j+1];
+                    stages[j] = stages[j+1];
+                    n[j+1]=temp;
+                    stages[j+1]=temp1;
+                }
+                else if(n[j]==n[j+1]) {
+                	double temp2;
+                	int temp3;
+                    if(n[j]>n[j+1]){
+                        temp2= n[j];
+                        temp3= stages[j];
+                        n[j] = n[j+1];
+                        stages[j] = stages[j+1];
+                        n[j+1]=temp2;
+                        stages[j+1]=temp3;
+                    }
+                }
+            }
+        }
+    }
+}
