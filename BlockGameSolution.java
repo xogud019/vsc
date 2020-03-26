36acc9d6db570a1daa1a1a889f74eb5c58e6130d
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-03-26 09:56:43 +0900

    commit

---
+++ BlockGameSolution.java
@@ -0,0 +1,41 @@
+public class BlockGameSolution{
+    
+    public static int solution(int[][] board){
+        int result =0;
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j>board[i].length; j++){
+
+            }
+        }
+        return result;
+    }
+
+    public static boolean upClear(int[][] board){
+        boolean isC = false;
+
+        return isC;
+    }
+    public static void main(String[] args){
+        int[][] board = {
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,4,0,0,0},
+            {0,0,0,0,0,4,4,0,0,0},
+            {0,0,0,0,3,0,4,0,0,0},
+            {0,0,0,2,3,0,0,0,5,5},
+            {1,2,2,2,3,3,0,0,0,5},
+            {1,1,1,0,0,0,0,0,0,5}};
+    }
+}
+
+/*
+1       1         1      1
+111     1       111     111
+        11       
+ 1
+ 1
+11
+
+*/
