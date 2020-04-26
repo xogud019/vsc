9b78f0f615c0565a73404771bf65afebbad8098f
xogud019 <57133962+xogud019@users.noreply.github.com>
2020-04-26 22:45:59 +0900

    commit

--- KeySolution.java
+++ KeySolution.java
@@ -11,7 +11,42 @@ public class KeySolution {
 
     public static boolean solution(int[][] lock, int[][] key){
         boolean isUnLock = false;
+        int[][] extendsLock = new int[lock.length*3][lock.length*3];
+        int[][] temp = extendsLock;
+        for(int i=lock.length; i<lock.length*2; i++){
+            for(int j=lock.length; j<lock.length*2; j++){
+                extendsLock[i][j] = lock[i-lock.length][j-lock.length];
+            }
+        }
 
+        for(int i=0; i<extendsLock.length-2; i++){
+            for(int j=0; j<extendsLock.length-2; j++){
+                int row=0, col =0;
+                for(int k=i; k<key.length+i;k++){
+                    for(int l= j; l<key.length+j; l++){
+                        if(temp[k][l]==0&&key[row][col]==1){
+                            temp[k][l]= key[row][col];
+                            col++;
+                        }
+                        else if(temp[k][l]==1&&key[row][col]==0){
+                            col++;
+                        }
+                        else{
+                            col++;
+                        }
+                    }
+                    col=0;
+                    row++;
+                }
+                if(keyCheck(temp)==true){
+                    return isUnLock = true;
+                }
+                else{
+                    temp = extendsLock;
+                }
+            }
+        }
+        //print(extendsLock);
         return isUnLock;
     }
     public static void print(int[][] array){
@@ -33,6 +68,23 @@ public class KeySolution {
         return rotateKey;
     }
 
+    public static boolean keyCheck(int[][] lock){
+        boolean isUnlock =false;
+        int count = 0;
+        for(int i=lock.length/3; i<lock.length/3*2; i++){
+            for(int j=lock.length/3; j<lock.length/3*2; j++){
+                if(lock[i][j]==1){
+                    count++;
+                }
+            }
+        }
+        if(count==(lock.length*lock.length)/9){
+            isUnlock = true;
+        }
+
+        return isUnlock;
+    }
+    /*
     public static int[][] rightMove(int[][] key){
         int[][] rightMoveArray = new int[key.length][key.length];
 
@@ -63,7 +115,8 @@ public class KeySolution {
         }
         return downMoveArray;
     }
-
+    */
+    /*
     public static boolean unlock(int[][] lock, int[][] key){
         boolean isUnlook = false;
         int count=0;
@@ -71,4 +124,5 @@ public class KeySolution {
         for(int i=0; i<)
         return isUnlook;
     }
+    */
 }
